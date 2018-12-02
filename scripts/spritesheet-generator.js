faf0753771cb06fa41dfc336fd06a388abdb45b6
Marc Solà Nadal <marcsolanadal@gmail.com>
2018-12-02 16:14:12 +0100

    Housekeeping

--- scripts/spritesheet-generator.js
+++
@@ -1,7 +0,0 @@
-var spritesheet = require('spritesheet-js');
-
-spritesheet('./assets/images/temp/*.png', {format: 'json'}, function (err) {
-  if (err) throw err;
-
-  console.log('spritesheet successfully generated');
-});
