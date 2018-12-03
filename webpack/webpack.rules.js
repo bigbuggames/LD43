b02b6f24127632b1ba7923024e5515789206e20d
Marc Solà Nadal <marcsolanadal@gmail.com>
2018-12-03 11:36:03 +0100

    Github pages support added

--- webpack/webpack.rules.js
+++ webpack/webpack.rules.js
@@ -9,9 +9,11 @@ module.exports = [
     use: ['file-loader']
   },
   {
+    test: /\.(mp3)$/,
+    use: ['file-loader']
+  },
+  {
     test: /\.(woff|woff2|eot|ttf|otf)$/,
-    use: [
-      'file-loader'
-    ]
+    use: ['file-loader']
   }
 ];
